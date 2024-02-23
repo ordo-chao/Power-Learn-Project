742b10f676b1d11279ec71bb03f94439cddac0c8
ordo-chao <160326107+ordo-chao@users.noreply.github.com>
2024-02-23 12:03:14 +0300

    Assignment Week one

---
+++ Python/User_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name: ").title()
+age = input("How old are you: ")
+location = input("Where do you live: ").title()
+
+print(f"My name is {name} , I am {age} old and i live in {location}")
